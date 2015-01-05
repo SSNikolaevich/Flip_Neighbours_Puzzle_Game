238b71bd533b913a5d4883f76ea4cb7da78f5ff6
Skuratovich Sergey <ssn6@yandex.ru>
2015-01-05 20:15:07 +0300

    replace tabs with spaces.

--- app/src/main/java/com/github/ssnikolaevich/flipgame/game/Game.java
+++ app/src/main/java/com/github/ssnikolaevich/flipgame/game/Game.java
@@ -2,112 +2,112 @@ package com.github.ssnikolaevich.flipgame.game;
 import java.util.*;
 
 public class Game {
-	private Layout mLayout;
+    private Layout mLayout;
     private OnTileFlipListener onTileFlipListener;
-	
-	public Game(int columns, int rows) {
-		mLayout = new Layout(columns, rows);
-        onTileFlipListener = null;
-		initLayout();
-	}
 
-	private void initLayout() {
-		int columns = mLayout.getColumns();
-		int rows = mLayout.getRows();
-		Random random = new Random();
-		for (int c = 0; c < columns; ++c) {
-			for (int r = 0; r < rows; ++r) {
-				Tile tile = mLayout.getTile(c, r);
-				tile.setFront(getRandomTileValue(random));
-				tile.setBack(getRandomTileValue(random));
-			}
-		}
-		int flipsCount = columns * rows;
-		for (int i = 0; (i < flipsCount) || isOver(); ++i) {
-			int column = random.nextInt(columns);
-			int row = random.nextInt(rows);
-			makeMove(column, row);
-		}
-	}
-	
-	private static Value getRandomTileValue(Random random) {
+    public Game(int columns, int rows) {
+        mLayout = new Layout(columns, rows);
+        onTileFlipListener = null;
+        initLayout();
+    }
+
+    private void initLayout() {
+        int columns = mLayout.getColumns();
+        int rows = mLayout.getRows();
+        Random random = new Random();
+        for (int c = 0; c < columns; ++c) {
+            for (int r = 0; r < rows; ++r) {
+                Tile tile = mLayout.getTile(c, r);
+                tile.setFront(getRandomTileValue(random));
+                tile.setBack(getRandomTileValue(random));
+            }
+        }
+        int flipsCount = columns * rows;
+        for (int i = 0; (i < flipsCount) || isOver(); ++i) {
+            int column = random.nextInt(columns);
+            int row = random.nextInt(rows);
+            makeMove(column, row);
+        }
+    }
+
+    private static Value getRandomTileValue(Random random) {
         Value value = new Value();
-		if (random.nextFloat() < 0.2f) {
+        if (random.nextFloat() < 0.2f) {
             value.setOther(true);
-		} else {
+        } else {
             while (value.empty()) {
                 value.setLeft(random.nextFloat() < 0.25f);
                 value.setRight(random.nextFloat() < 0.25f);
                 value.setTop(random.nextFloat() < 0.25f);
                 value.setBottom(random.nextFloat() < 0.25f);
             }
-		}
+        }
         return value;
-	}
-	
-	public boolean isOver() {
-		int columns = mLayout.getColumns();
-		int rows = mLayout.getRows();
-		for (int c = 0; c < columns; ++c) {
-			for (int r = 0; r < rows; ++r) {
-				if (mLayout.getTile(c, r).getVisibleSide() != Tile.FRONT)
-					return false;
-			}
-		}
-		return true;
-	}
-	
-	public int getColumns() {
-		return mLayout.getColumns();
-	}
-	
-	public int getRows() {
-		return mLayout.getRows();
-	}
-	
-	public Tile getTile(int column, int row) {
-		return mLayout.getTile(column, row);
-	}
-	
-	private void flipTile(int column, int row) {
+    }
+
+    public boolean isOver() {
+        int columns = mLayout.getColumns();
+        int rows = mLayout.getRows();
+        for (int c = 0; c < columns; ++c) {
+            for (int r = 0; r < rows; ++r) {
+                if (mLayout.getTile(c, r).getVisibleSide() != Tile.FRONT)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public int getColumns() {
+        return mLayout.getColumns();
+    }
+
+    public int getRows() {
+        return mLayout.getRows();
+    }
+
+    public Tile getTile(int column, int row) {
+        return mLayout.getTile(column, row);
+    }
+
+    private void flipTile(int column, int row) {
         Tile tile = mLayout.getTile(column, row);
         tile.flip();
         if (onTileFlipListener != null)
             onTileFlipListener.onFlip(this, column, row);
-	}
-	
-	public void makeMove(int column, int row) {
-		Tile tile = mLayout.getTile(column, row);
-		Value value = tile.getVisibleValue();
-		int columns = mLayout.getColumns();
-		int rows = mLayout.getRows();
-		if (value.isOther()) {
-			for (int c = 0; c < columns; ++c) {
-				for (int r = 0; r < rows; ++r) {
-					if ((c != column) && (r != row)) {
-						flipTile(c, r);
-					}
-				}
-			}
-		} else {
-			if (value.isLeft()) {
-				int c = ((column > 0)? column : columns) - 1;
-				flipTile(c, row);
-			}
-			if (value.isRight()) {
-				int c = ((column + 1) < columns)? (column + 1) : 0;
-				flipTile(c, row);
-			}
-			if (value.isTop()) {
-				int r = ((row > 0)? row : rows) - 1;
-				flipTile(column, r);
-			}
-			if (value.isBottom()) {
-				int r = ((row + 1) < rows)? (row + 1) : 0;
-				flipTile(column, r);
-			}
-		}
-	}
+    }
+
+    public void makeMove(int column, int row) {
+        Tile tile = mLayout.getTile(column, row);
+        Value value = tile.getVisibleValue();
+        int columns = mLayout.getColumns();
+        int rows = mLayout.getRows();
+        if (value.isOther()) {
+            for (int c = 0; c < columns; ++c) {
+                for (int r = 0; r < rows; ++r) {
+                    if ((c != column) && (r != row)) {
+                        flipTile(c, r);
+                    }
+                }
+            }
+        } else {
+            if (value.isLeft()) {
+                int c = ((column > 0)? column : columns) - 1;
+                flipTile(c, row);
+            }
+            if (value.isRight()) {
+                int c = ((column + 1) < columns)? (column + 1) : 0;
+                flipTile(c, row);
+            }
+            if (value.isTop()) {
+                int r = ((row > 0)? row : rows) - 1;
+                flipTile(column, r);
+            }
+            if (value.isBottom()) {
+                int r = ((row + 1) < rows)? (row + 1) : 0;
+                flipTile(column, r);
+            }
+        }
+    }
 
     public void setOnTileFlipListener(OnTileFlipListener listener) {
         onTileFlipListener = listener;
--- app/src/main/java/com/github/ssnikolaevich/flipgame/game/Tile.java
+++ app/src/main/java/com/github/ssnikolaevich/flipgame/game/Tile.java
@@ -2,60 +2,60 @@ package com.github.ssnikolaevich.flipgame.game;
 
 public class Tile {
     public static final int FRONT = 0;
-	public static final int BACK = 1;
+    public static final int BACK = 1;
 
-	private Value mFront;
-	private Value mBack;
-	private int mSide;
+    private Value mFront;
+    private Value mBack;
+    private int mSide;
 
-	Tile() {
-		mFront = new Value();
-		mBack = new Value();
-		mSide = FRONT;
-	}
+    Tile() {
+        mFront = new Value();
+        mBack = new Value();
+        mSide = FRONT;
+    }
 
-	public Value getFront() {
-		return mFront;
-	}
+    public Value getFront() {
+        return mFront;
+    }
 
-	public Value getBack() {
-		return mBack;
-	}
+    public Value getBack() {
+        return mBack;
+    }
 
-	public void setFront(Value value) {
-		mFront = value;
-	}
+    public void setFront(Value value) {
+        mFront = value;
+    }
 
-	public void setBack(Value value) {
-		mBack = value;
-	}
+    public void setBack(Value value) {
+        mBack = value;
+    }
 
-	public int getVisibleSide() {
-		return mSide;
-	}
+    public int getVisibleSide() {
+        return mSide;
+    }
 
-	public void setVisibleSide(int side) {
-		mSide = side;
-	}
+    public void setVisibleSide(int side) {
+        mSide = side;
+    }
 
-	public Value get(int side) {
-		return (side == FRONT) ? mFront : mBack;
-	}
-	
-	public Value getVisibleValue() {
-		return get(getVisibleSide());
-	}
+    public Value get(int side) {
+        return (side == FRONT) ? mFront : mBack;
+    }
 
-	public void set(int side, Value value) {
-		if (side == FRONT) {
-			mFront = value;
-		} else {
-			mBack = value;
-		}
-	}
+    public Value getVisibleValue() {
+        return get(getVisibleSide());
+    }
 
-	public void flip() {
-		mSide = (mSide == FRONT) ? BACK : FRONT;
-	}
+    public void set(int side, Value value) {
+        if (side == FRONT) {
+            mFront = value;
+        } else {
+            mBack = value;
+        }
+    }
+
+    public void flip() {
+        mSide = (mSide == FRONT) ? BACK : FRONT;
+    }
 }
 
